0ec7089c998c603c6d1c14a1111e5f165395594d
Prasanna <gramopadhye37@gmail.com>
2023-03-26 12:45:15 +0530

    Trail Stage

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,6 +1,11 @@
+let play = true
+
 function toogleVideo(e) {
-    console.log(e.parentNode.parentNode)
     let video = e.parentNode.parentNode.children[0]
     if(video.paused) video.play()
     else video.pause()
+
+    
+    e.children[0].src = play ? '/assets/playButton.png' : '/assets/pauseButton.png'
+    play = !play
 }
